98cdc7a30d7c3a205ba0396bb73696b3b381927b
KEENTO <handball0321@gmail.com>
2023-01-02 16:37:07 -0800

    Added styled-components plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,28 @@
 import type { GatsbyConfig } from 'gatsby';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
-  plugins: [`gatsby-plugin-pnpm`],
+  plugins: [
+    `gatsby-plugin-pnpm`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    },
+    // {
+    //   resolve: `gatsby-source-openweathermap`,
+    //   options: {
+    //     apikey: process.env.GATSBY_OPEN_WEATHER_API_KEY,
+    //     location: 'York',
+    //     units: 'metric',
+    //     type: 'forecast',
+    //   },
+    // },
+  ],
   jsxRuntime: `automatic`,
 };
 
