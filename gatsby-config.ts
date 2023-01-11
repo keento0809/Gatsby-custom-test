9046f40c2a77e1cbb1da0366636879d2271f9570
KEENTO <handball0321@gmail.com>
2023-01-11 15:37:29 -0800

    Playing around source-plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,11 @@ dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+console.log(
+  process.env.GATSBY_SPOTIFY_CLIENT_ID,
+  process.env.GATSBY_SPOTIFY_REFRESH_TOKEN,
+);
+
 const config: GatsbyConfig = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
@@ -20,7 +25,7 @@ const config: GatsbyConfig = {
       resolve: `gatsby-source-openweathermap`,
       options: {
         apikey: process.env.GATSBY_OPEN_WEATHER_API_KEY,
-        location: 'Vancouver',
+        location: '*',
         units: 'metric',
         type: 'forecast',
       },
@@ -28,9 +33,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-spotify`,
       options: {
-        clientId: `<CLIENT_ID>`,
-        clientSecret: `<CLIENT_SECRET>`,
-        refreshToken: `<REFRESH_TOKEN>`,
+        clientId: process.env.GATSBY_SPOTIFY_CLIENT_ID,
+        clientSecret: process.env.GATSBY_SPOTIFY_CLIENT_SECRET,
+        refreshToken: process.env.GATSBY_SPOTIFY_REFRESH_TOKEN,
 
         fetchPlaylists: true, // optional. Set to false to disable fetching of your playlists
         fetchRecent: true, // optional. Set to false to disable fetching of your recently played tracks
